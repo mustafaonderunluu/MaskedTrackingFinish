d5671c7ca36a828d7b5972d5da598bbb261a7986
Mustafa Önder ÜNLÜ <116373504+mustafaonderunluu@users.noreply.github.com>
2024-02-11 15:06:09 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,36 @@
+﻿
+using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    // interfaceleri kullanacagimiz zaman ilk başta field dediğimiz interface türünde değişken tanımlarız
+    // bunuda global alanda ulaşabilelim diye yapıyoruz
+    // --buraya dikkat-- constructorlar newlendiğinde çaılışır o yüzden set ederiz ve interfaceler referans türlüdür.
+    // sonra istediğimiz method ya da classta kullanırız mesela 18. satirda 
+    public class PttManager : ISupplierService
+    {
+        private IApplicantService _applicantService;
+
+        public PttManager(IApplicantService applicantService) // Constructor new yapildiginda yapilir
+        {
+            _applicantService = applicantService;
+        }
+
+        public void GiveMask(Person person)
+        {
+            if (_applicantService.CheckPerson(person) ) {
+                Console.WriteLine( person.FirstName + "Maske verildi" );
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName+" İçin maske VERİLEMEZ! ");
+            }
+        }
+    }
+}
---
+++ WorkAround/Program.cs
@@ -0,0 +1,82 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.FirstName = "MUSTAFA ÖNDER";
+        person.LastName = "ÜNLÜ";
+        person.NationalIdentitiy = 1651;
+        person.DateOfBirthYear = 2002;
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person);
+
+        //Console.WriteLine("Merhabalar");
+
+        //Vatandas vatandas1 = new Vatandas();
+
+        //string mesaj = "Hi";
+
+        //if (string.IsNullOrEmpty(mesaj))
+        //    Console.WriteLine("Boş");
+        //else Console.WriteLine("Hi");
+
+        ////degiskenler();
+
+        //static void degiskenler()
+        //{
+        //    string mesaj2 = "merhaba";
+        //    double tutar = 0; // databaseden gelecek
+        //    int sayi = 0;
+        //    bool isLogin = false;
+
+        //    string ad = "asds";
+        //    string soyad = "123";
+        //    long tcNo = 12345678900;
+        //    int DogumYili = 1985;
+
+        //    Console.WriteLine(tutar * 1.18);
+        //    Console.WriteLine(tutar * 1.18);
+        //}
+        //static int topla(int sayi1, int sayi2)
+        //{
+        //    int sonuc = sayi1 + sayi2;
+        //    //Console.WriteLine(sonuc.ToString());
+        //    return sonuc;
+        //}
+        //static void SelamVer(string mesaj)
+        //{
+        //    Console.WriteLine(mesaj + " Merhaba, toplam = " + topla(1, 4));
+        //}
+        //static void SelamVera(string mesaj)
+        //{
+        //    Console.WriteLine(mesaj + " Merhaba, toplam = " + topla(17, 4));
+        //}
+        ////topla(sayi1: 3, sayi2:1) ;
+        //SelamVer(mesaj);
+        //SelamVera(mesaj);
+
+
+        ////garbage Collector
+        ////array, class, abstrack, interface referans tpli metodlar
+        ////int, double, bool, float sayı değer tipli değişkenlerdir.
+
+        //string[] sehirler1 = new[] { "Ankara", "İzmir", "İstanbul" };
+        //string[] sehirler2 = new[] { "Bursa", "Antalya", "Diyarbakır" };
+
+        //sehirler2 = sehirler1; //burada olan şey sehirler2 sehirler1 in referansıdır
+        //sehirler1[0] = "Adana";
+        //Console.WriteLine(sehirler2[0]);// sonuç adana
+
+        //int sayi1 = 20;
+        //int sayi2 = 10;
+        //sayi2 = sayi1;
+        //sayi1 = 30;
+        //Console.WriteLine(sayi2);
+
+
+    }
+}
